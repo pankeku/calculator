bf732ff40cb7a719ea395ea38886a423ecea441e
pankeku <drwagw@gmail.com>
2022-06-03 19:03:35 +0300

    All operations and functions working

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const numbers = document.querySelectorAll('.numbers');
 const input = document.querySelector('.calculator__input');
 const operators = document.querySelectorAll('.operator');
 const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const plusMinus = document.querySelector('.plus-minus');
+const percentage = document.querySelector('.percentage');
+const dot = document.querySelector('.dot');
+const backspace = document.querySelector('.backspace');
+
 let firstNumber = '';
 let secondNumber = '';
-let evaluationAllowed = false;
-
-input.textContent = '';
+let cache = '';
+let operator = '';
 
 function operate(n1, n2, operator) {
   if (operator === '+') {
@@ -26,80 +31,136 @@ function operate(n1, n2, operator) {
   }
 }
 
+function print() {
+  console.log('First: ' + firstNumber);
+  console.log('Second: ' + secondNumber);
+  console.log('Cache: ' + cache);
+  console.log('Operator: ' + operator);
+}
+
 numbers.forEach((button) => {
   button.addEventListener('click', () => {
-    if (secondNumber !== '') {
-      secondNumber = '';
-      input.textContent = '';
-    }
+    cache = cache + button.textContent;
 
-      
-    updateDisplay(button.textContent);
-
+    updateDisplay();
   });
 });
 
 operators.forEach((button) => {
   button.addEventListener('click', () => {
-
     setNumbers();
-
     evaluate();
 
-    selectedOperator = button.textContent;
-
+    operator = button.textContent;
   });
 });
 
 equal.addEventListener('click', () => {
-
   setNumbers();
   evaluate();
+});
 
+clear.addEventListener('click', () => {
+  clearEverything();
 });
 
 
-function setNumbers() {
 
-  if (firstNumber === '') {
-    firstNumber = input.textContent;
-  } else if (secondNumber === '' && input.textContent != '') {
-    secondNumber = input.textContent;    
-    evaluationAllowed = true;
-  }
-
-  clearDisplay();
-}
-
-function evaluate() {
-
-  if (firstNumber !== '' && secondNumber !== '' && evaluationAllowed === true) {
-
-    let result = getResult();
-
-    input.textContent = '';
-
-    updateDisplay(result);
-
-    firstNumber = result;
-
-    evaluationAllowed = false;
-  }
-}
-
-function updateDisplay(text) {  
-  input.textContent += text;
-}
-
-function clearDisplay() {
-  
+function clearEverything() {
+  firstNumber = '';
+  secondNumber = '';
+  cache = '';
+  operator = '';
   input.textContent = '';
 }
 
-function getResult() {
-  return operate(
-    parseInt(firstNumber),
-    parseInt(secondNumber),
-    selectedOperator
-  );
+plusMinus.addEventListener('click', () => {
+  cache = input.textContent;
+  if (cache !== '' && cache !== '0') {
+    if (cache.charAt(0) !== '-') {
+      cache = '-' + cache;
+    } else {
+      cache = cache.substring(1);
+    }
+    updateDisplay();
+  }
+});
+
+percentage.addEventListener('click', () => {
+  cache = parseFloat(input.textContent) / 100;
+
+  updateDisplay();
+});
+
+dot.addEventListener('click', () => {
+  cache = input.textContent;
+  if (!(cache.includes('.'))) {
+    cache = cache + '.';
+    updateDisplay();
+  }
+})
+
+backspace.addEventListener('click', () => {
+  cache = input.textContent;
+  cache = cache.substring(0, cache.length - 1);
+  console.log(cache);
+  updateDisplay();
+})
+
+function divideByZero() {
+  if (operator === '/' && secondNumber === '0') {
+    return true;
+  }
+
+  return false;
+}
+
+function evaluate() {
+  if (divideByZero()) {
+    clearEverything();
+    input.textContent = 'Bruh..';
+  }
+
+  if (firstNumber != '' && secondNumber != '') {
+    if (divideByZero) {
+    }
+
+    let result = operate(
+      parseFloat(firstNumber),
+      parseFloat(secondNumber),
+      operator
+    );
+
+    result = +result.toFixed(5);
+
+    cache = result;
+
+    updateDisplay();
+
+    firstNumber = result;
+    secondNumber = '';
+    cache = '';
+    operator = '';
+  }
+}
+
+function updateDisplay() {
+  input.textContent = cache;
+}
+
+function setNumbers() {
+  if (firstNumber !== '' && secondNumber === '' && operator !== '') {
+    secondNumber = cache;
+    cache = '';
+  }
+
+  if (firstNumber !== '' && operator === '' && cache !== '') {
+    firstNumber = cache;
+    cache = '';
+  }
+
+  if (firstNumber === '') {
+    firstNumber = cache;
+    cache = '';
+  }
 }
